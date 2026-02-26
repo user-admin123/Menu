8d31496ef1cd57aa84e6c6f0b069a45dd9b59dd5
user-admin123 <600001raj@gmail.com>
2026-02-26 22:48:01 +0530

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  'https://YOUR_PROJECT_URL.supabase.co',
-  'YOUR_SUPABASE_ANON_KEY'
+  'https://szacwcwggrsjipmmwqbt.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6YWN3Y3dnZ3JzamlwbW13cWJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMjE2NTcsImV4cCI6MjA4NzY5NzY1N30.StzDoZgHlqIC3y3-bZ532i547iU2n54WhatHl8XYadA'
 );
