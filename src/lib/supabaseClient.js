4fb0f96873947775f9d433bb63d43bb2aeae708c
user-admin123 <600001raj@gmail.com>
2026-02-27 22:16:56 +0530

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,11 +1,8 @@
-// lib/supabaseClient.js
-
 import { createClient } from '@supabase/supabase-js';
 
-// Replace these with your own Supabase project details
-const supabaseUrl = 'https://kktfairmpogwlitxqeag.supabase.co'; 
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtrdGZhaXJtcG9nd2xpdHhxZWFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyMDU0NjgsImV4cCI6MjA4Nzc4MTQ2OH0.6QRvusPQnyRA913WL9FftVQJ_2UY8xh0UWLZmI2CQSE'; 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default supabase;
+export default supabase
