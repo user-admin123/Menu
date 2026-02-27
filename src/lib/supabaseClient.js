815a26c1985117a0e3babd4c9e538d23da411e11
user-admin123 <600001raj@gmail.com>
2026-02-27 12:28:55 +0530

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,11 @@
-import { createClient } from "@supabase/supabase-js";
+// lib/supabaseClient.js
 
-// Your Supabase URL and Key (get them from your Supabase dashboard)
-const supabaseUrl = "https://szacwcwggrsjipmmwqbt.supabase.co"; // Replace with your Supabase URL
-const supabaseKey = "sb_publishable_HbsR3S5x3Vap9oSesSWk_w_32N0VxYO"; // Replace with your Supabase public key
-export const supabase = createClient(supabaseUrl, supabaseKey);
+import { createClient } from '@supabase/supabase-js';
+
+// Replace these with your own Supabase project details
+const supabaseUrl = 'https://szacwcwggrsjipmmwqbt.supabase.co'; 
+const supabaseAnonKey = 'sb_publishable_HbsR3S5x3Vap9oSesSWk_w_32N0VxYO'; 
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export default supabase;
